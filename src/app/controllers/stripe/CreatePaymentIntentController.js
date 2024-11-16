91c0fa46505ef0d0449bba0f60be5a47d3a2bc74
Theimis <theimislimalopes422@gmail.com>
2024-11-16 18:27:39 -0300

    api-finalizado

--- src/app/controllers/stripe/CreatePaymentIntentController.js
+++ src/app/controllers/stripe/CreatePaymentIntentController.js
@@ -1,17 +1,16 @@
 import Stripe from "stripe";
 import * as Yup from "yup";
 
-
 const stripe = new Stripe(
   "sk_test_51QKf9BAK9EZcPjY6Oo2TuenZx8kAlv1iJ7ZwTw6L4VXSv2pWkg8uMGt2Qx6ewO70ErMdt5Ilp1PEJ6LfAVKO9CCu006X1mnU6y",
-); 
+);
 
 const calculateOrderAmount = (items) => {
   const total = items.reduce((acc, current) => {
     return acc + current.price * current.quantity;
   }, 0);
 
-  return total * 100;
+  return total;
 };
 
 class CreatePaymentIntentController {
@@ -27,6 +26,7 @@ class CreatePaymentIntentController {
           }),
         ),
     });
+
     try {
       schema.validateSync(request.body, { abortEarly: false });
     } catch (err) {
@@ -34,7 +34,6 @@ class CreatePaymentIntentController {
     }
 
     const { products } = request.body;
-
     const amount = calculateOrderAmount(products);
 
     const paymentIntent = await stripe.paymentIntents.create({
